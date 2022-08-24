39950748eb34c2a2da143b56ccb7b76b2a9d1eb2
muhammed <Mukhammadnr6685@gmail.com>
2022-08-24 17:59:29 +0300

    added dashbord

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from passlib.hash import sha256_crypt
 from wtforms import Form, PasswordField, StringField, TextAreaField, validators
 
 from data import Articles
-
 app = Flask(__name__)
 Articles=Articles()
 app.config['MYSQL_HOST']='localhost'
@@ -50,9 +49,11 @@ class RegisterForm (Form):
         validators.EqualTo('confirm', message='PasswordS do not match')
     }) 
     confirm=PasswordField('Confirm Password')
+
 @app.route('/register',methods={'GET','POST'})
 def register ():
     form = RegisterForm(request.form)
+    
     if request.method=='POST'and form.validate():
         name = form.name.data
         email=form.email.data
@@ -63,6 +64,7 @@ def register ():
         cur.execute('INSERT INTO users (name,email,username,password)VALUES(%s,%s,%s,%s)',(name,email,username,password))
         mysql.connection.commit()
         cur.close()
+        
         flash("you are now registered and can log in","success")
 
         redirect(url_for('index'))
@@ -75,14 +77,18 @@ def register ():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    
+    
     if request.method=='POST':
         username = request.form['username']
         password_candidate=request.form['password']
         cur=mysql.connection.cursor()
         result=cur.execute("SELECT * FROM users WHERE username=%s",[username])
+        
+        
         if result > 0:
-            data = cur.ferchone()
-            password = date ['password']
+            data = cur.fetchone()
+            password = data ['password']
 
             
             if sha256_crypt.verify(password_candidate,password):
@@ -103,10 +109,31 @@ def login():
             error='Username not found'
             return render_template('login.html',error=error)
     
-    
-    
     return render_template('login.html')
 
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if 'logged_in'in session:
+            return f(*args,**kwargs)
+        else:
+            flash('Unauthorized,please login','danger')
+            return redirect(url_for(login))
+    return wrap
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('uou are now logged out','success')
+    return redirect(url_for('login'))
+
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__=='__main__':
